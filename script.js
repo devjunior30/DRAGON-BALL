2280b35b956a629c7270cb2a0b72a0498e8c3adf
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2023-11-13 15:42:12 -0300

    parte de html e script finalizada, iniciando parte de transição e responsividade.

---
+++ script.js
@@ -0,0 +1,29 @@
+let time = 2000,
+        currentImageIndex = 0,
+        image = document.querySelectorAll('section img'),
+
+    max = image.length;
+
+    function nextImage() {
+
+        
+
+        
+
+    }
+
+    function start() {
+      setInterval(() => {
+        image[currentImageIndex].classList.remove("personagens")
+
+        currentImageIndex ++
+
+        if(currentImageIndex >= max) {
+            currentImageIndex = 0
+        }
+
+        image[currentImageIndex].classList.add("personagens")
+      }, time);
+    }
+
+    window.addEventListener("load", start)

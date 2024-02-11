bbced5e21f8dbc5e570c8d883c48f1b43e9ef61e
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2024-02-11 19:46:05 -0300

    esqueci de ativar o script

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-// let time = 3000,
-//   currentImageIndex = 0,
-//   image = document.querySelectorAll('section img'),
+let time = 3000,
+  currentImageIndex = 0,
+  image = document.querySelectorAll('section img'),
 
-//   max = image.length;
+  max = image.length;
 
-// function nextImage() {
-
-
-// }
-
-// function start() {
-//   setInterval(() => {
-//     image[currentImageIndex].classList.remove("personagens")
-
-//     currentImageIndex++
-
-//     if (currentImageIndex >= max) {
-//       currentImageIndex = 0
-//     }
-
-//     image[currentImageIndex].classList.add("personagens")
-//   }, time);
+function nextImage() {
 
 
-//   let characters = ["Gogeta",
-//     "Freeza",
-//     "Gohan",
-//     "Goku",
-//     "Majin Boo",
-//     "Piccolo",
-//     "Vegetta",
-//     "Vegetto"]
+}
 
-//   let indice = 0
+function start() {
+  setInterval(() => {
+    image[currentImageIndex].classList.remove("personagens")
 
-//   function exibirNome() {
-//     document.querySelector("h2").innerHTML = characters[indice];
+    currentImageIndex++
 
-//     indice = (indice + 1) % characters.length;
+    if (currentImageIndex >= max) {
+      currentImageIndex = 0
+    }
 
-//   }
+    image[currentImageIndex].classList.add("personagens")
+  }, time);
 
-//   exibirNome()
 
-//   setInterval(exibirNome, 3000)
+  let characters = ["Gogeta",
+    "Freeza",
+    "Gohan",
+    "Goku",
+    "Majin Boo",
+    "Piccolo",
+    "Vegetta",
+    "Vegetto"]
 
-// }
-// window.addEventListener("load", start)
+  let indice = 0
+
+  function exibirNome() {
+    document.querySelector("h2").innerHTML = characters[indice];
+
+    indice = (indice + 1) % characters.length;
+
+  }
+
+  exibirNome()
+
+  setInterval(exibirNome, 3000)
+
+}
+window.addEventListener("load", start)
 
 function mostrarTexto() {
 

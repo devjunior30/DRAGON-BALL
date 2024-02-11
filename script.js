8bd8f55971070790bd5905aba8e6594d23d856d9
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2024-02-11 19:42:55 -0300

    novas atualização na estilização da pagina, centralizando os elementos na parte  central da pagina.

--- script.js
+++ script.js
@@ -1,49 +1,76 @@
-let time = 3000,
-  currentImageIndex = 0,
-  image = document.querySelectorAll('section img'),
+// let time = 3000,
+//   currentImageIndex = 0,
+//   image = document.querySelectorAll('section img'),
 
-  max = image.length;
+//   max = image.length;
 
-function nextImage() {
-
-
-}
-
-function start() {
-  setInterval(() => {
-    image[currentImageIndex].classList.remove("personagens")
-
-    currentImageIndex++
-
-    if (currentImageIndex >= max) {
-      currentImageIndex = 0
-    }
-
-    image[currentImageIndex].classList.add("personagens")
-  }, time);
+// function nextImage() {
 
 
-  let characters = ["Gogeta",
-    "Freeza",
-    "Gohan",
-    "Goku",
-    "Majin Boo",
-    "Piccolo",
-    "Vegetta",
-    "Vegetto"]
+// }
 
-  let indice = 0
+// function start() {
+//   setInterval(() => {
+//     image[currentImageIndex].classList.remove("personagens")
 
-  function exibirNome() {
-    document.querySelector("h2").innerHTML = characters[indice];
+//     currentImageIndex++
 
-    indice = (indice + 1) % characters.length;
+//     if (currentImageIndex >= max) {
+//       currentImageIndex = 0
+//     }
 
+//     image[currentImageIndex].classList.add("personagens")
+//   }, time);
+
+
+//   let characters = ["Gogeta",
+//     "Freeza",
+//     "Gohan",
+//     "Goku",
+//     "Majin Boo",
+//     "Piccolo",
+//     "Vegetta",
+//     "Vegetto"]
+
+//   let indice = 0
+
+//   function exibirNome() {
+//     document.querySelector("h2").innerHTML = characters[indice];
+
+//     indice = (indice + 1) % characters.length;
+
+//   }
+
+//   exibirNome()
+
+//   setInterval(exibirNome, 3000)
+
+// }
+// window.addEventListener("load", start)
+
+function mostrarTexto() {
+
+  const texto = document.querySelector("p");
+  let button = document.querySelector("button")
+
+  if (texto.classList.contains("escondido")) {
+    texto.classList.remove("escondido")
+
+  } else {
+    texto.classList.add("escondido")
   }
 
-  exibirNome()
+  if (button.textContent === "Mostrar Texto") {
+    button.textContent = "Ocultar Texto";
 
-  setInterval(exibirNome, 3000)
+  } else {
+    button.textContent = "Mostrar Texto";
+  }
+
+
+
 
 }
-window.addEventListener("load", start)
+
+let button = document.querySelector("button").addEventListener("click",
+  mostrarTexto);

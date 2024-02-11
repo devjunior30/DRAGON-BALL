68066b75481265ed1a7e351e9807c1af58cbd970
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2024-02-11 10:06:54 -0300

    atualizando a pagina para colocar nomes dos personagens assim que ocorrer a troca de imagens

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let time = 3000,
-        currentImageIndex = 0,
-        image = document.querySelectorAll('section img'),
+// let time = 3000,
+//         currentImageIndex = 0,
+//         image = document.querySelectorAll('section img'),
 
-    max = image.length;
+//     max = image.length;
 
-    function nextImage() {
-
-        
+//     function nextImage() {
 
         
 
-    }
+        
 
-    function start() {
-      setInterval(() => {
-        image[currentImageIndex].classList.remove("personagens")
+//     }
 
-        currentImageIndex ++
+//     function start() {
+//       setInterval(() => {
+//         image[currentImageIndex].classList.remove("personagens")
 
-        if(currentImageIndex >= max) {
-            currentImageIndex = 0
-        }
+//         currentImageIndex ++
 
-        image[currentImageIndex].classList.add("personagens")
-      }, time);
-    }
+//         if(currentImageIndex >= max) {
+//             currentImageIndex = 0
+//         }
 
-    window.addEventListener("load", start)
+//         image[currentImageIndex].classList.add("personagens")
+//       }, time);
+//     }
+
+//     window.addEventListener("load", start)
